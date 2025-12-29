d950eb0ce4c9759da9e2686b2423462c1c12ebaf
Lyna <40660493+0x4c756e61@users.noreply.github.com>
2025-12-29 13:56:06 +0100

    Zen: Patch with sine mod

--- home.nix
+++ home.nix
@@ -9,7 +9,32 @@
   zen-browser,
   ...
 }:
+let
+  custom-zen =
+    zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.beta-unwrapped.overrideAttrs
+      (oldAttrs: rec {
+        libName = "zen-bin-1.17.15b";
+        fsautoconfig = (
+          builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/MrOtherGuy/fx-autoconfig/master/program/config.js";
+            sha256 = "1mx679fbc4d9x4bnqajqx5a95y1lfasvf90pbqkh9sm3ch945p40";
+          }
+        );
+        configpref = (
+          builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/MrOtherGuy/fx-autoconfig/refs/heads/master/program/defaults/pref/config-prefs.js";
+            sha256 = "sha256-a/0u0TnRj/UXjg/GKjtAWFQN2+ujrckSwNae23DBfs4=";
+          }
+        );
 
+        postInstall = (oldAttrs.postInstall or "") + ''
+          chmod -R u+w "$out/lib/${libName}"
+          cp "${fsautoconfig}" "$out/lib/${libName}/config.js"
+          mkdir -p "$out/lib/${libName}/defaults/pref"
+          cp "${configpref}" "$out/lib/${libName}/defaults/pref/config-pref.js"
+        '';
+      });
+in
 {
   home.username = "luna";
   home.homeDirectory = "/home/luna";
@@ -115,15 +140,9 @@
   # Zen beta, best firefox fork
   programs.zen-browser = {
     enable = true;
-    package = (config.lib.nixGL.wrap zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.beta);
-    extraPrefsFiles = [ # in theory, this should be installing fx-autoconfig and allow Sine/Cosine to work, but it does not
-      (builtins.fetchurl {
-        url = "https://raw.githubusercontent.com/MrOtherGuy/fx-autoconfig/master/program/config.js";
-        sha256 = "1mx679fbc4d9x4bnqajqx5a95y1lfasvf90pbqkh9sm3ch945p40";
-      })
-    ];
+    package = (config.lib.nixGL.wrap ((pkgs.wrapFirefox) custom-zen { }));
   };
 
-  warnings = ["Check if there was any progress regarding zen-flake support for Sine"];
+  warnings = [ "Check if there was any progress regarding zen-flake support for Sine" ];
 
 }
