59a7e3464396e51f82644049d3f8ce125917fe80
Lyna <40660493+0x4c756e61@users.noreply.github.com>
2026-07-25 20:11:04 +0200

    feat: no more pisscord
    thanks @moonlight-mod for making pisscord useable

--- hosts/rover/dotfiles/social.nix
+++ hosts/rover/dotfiles/social.nix
@@ -6,16 +6,16 @@
 }: {
   home.packages = with pkgs; [
     (config.lib.nixGL.wrap signal-desktop)
-    (config.lib.nixGL.wrap dissent)
-    (config.lib.nixGL.wrap (
-      discord.override {
-        withMoonlight = true;
-        # withOpenASAR = true;
-        moonlight = moonlight.packages.${pkgs.stdenv.hostPlatform.system}.moonlight;
-        desktopName = "moonlight";
-        commandLineArgs = "--ozone-platform=wayland"; # Seems to be required on non-nixOS installations? Not sure since the package already appends this if NIXOS_WAYLAND is set
-      }
-    ))
+    # (config.lib.nixGL.wrap dissent)
+    # (config.lib.nixGL.wrap (
+    #   discord.override {
+    #     withMoonlight = true;
+    #     # withOpenASAR = true;
+    #     moonlight = moonlight.packages.${pkgs.stdenv.hostPlatform.system}.moonlight;
+    #     desktopName = "moonlight";
+    #     commandLineArgs = "--ozone-platform=wayland"; # Seems to be required on non-nixOS installations? Not sure since the package already appends this if NIXOS_WAYLAND is set
+    #   }
+    # ))
 
   ];
 }
